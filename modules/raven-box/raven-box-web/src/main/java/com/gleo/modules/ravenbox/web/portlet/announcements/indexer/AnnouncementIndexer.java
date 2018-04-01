f3ee9ced34bdeb94c4ab43f168b69e4ebaf7c6aa
Guillaume Lenoir <guillaume.lenoir@gleo.fr>
2018-04-01 13:59:58 +0200

    update announcement indexer

--- modules/raven-box/raven-box-web/src/main/java/com/gleo/modules/ravenbox/web/portlet/announcements/indexer/AnnouncementIndexer.java
+++ modules/raven-box/raven-box-web/src/main/java/com/gleo/modules/ravenbox/web/portlet/announcements/indexer/AnnouncementIndexer.java
@@ -15,7 +15,7 @@ import com.gleo.modules.ravenbox.constants.RavenBoxPortletKeys;
 import com.gleo.modules.ravenbox.model.Announcement;
 import com.gleo.modules.ravenbox.service.AnnouncementLocalService;
 import com.liferay.asset.kernel.model.AssetCategory;
-import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.message.boards.kernel.service.MBMessageLocalServiceUtil;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
@@ -36,6 +36,7 @@ import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
@@ -56,50 +57,55 @@ import com.liferay.ratings.kernel.service.RatingsStatsLocalServiceUtil;
 public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 
 	@Reference
-	protected void setAnnouncementLocalService(
-			AnnouncementLocalService announcementLocalService) {
+	protected void setAnnouncementLocalService(AnnouncementLocalService announcementLocalService) {
 		this.announcementLocalService = announcementLocalService;
 	}
-	
+
 	@Reference
-	protected void setIndexWriterHelper(
-			IndexWriterHelper indexWriterHelper) {
+	protected void setIndexWriterHelper(IndexWriterHelper indexWriterHelper) {
 		this.indexWriterHelper = indexWriterHelper;
 	}
+
+	@Reference
+	protected void setAssetCategoryLocalService(AssetCategoryLocalService assetCategoryLocalService) {
+		this.assetCategoryLocalService = assetCategoryLocalService;
+	}
+
+	private AssetCategoryLocalService assetCategoryLocalService;
 
 	/**
 	 * AnnouncementService
 	 */
 	private AnnouncementLocalService announcementLocalService;
-	
+
 	/**
 	 * IndexWriterHelper
 	 */
 	protected IndexWriterHelper indexWriterHelper;
 
 	private static final Log LOGGER = LogFactoryUtil.getLog(AnnouncementIndexer.class);
-	
+
 	/**
 	 * Announcement Classname
 	 */
 	public static final String CLASS_NAME = Announcement.class.getName();
-	
+
 	/**
 	 * Announcement PORTLET_ID
 	 */
 	public static final String PORTLET_ID = RavenBoxPortletKeys.ANNOUNCEMENTS_CONFIGURATION;
-	
-	public static final boolean ASSET_CATEGORIES_SEARCH_HIERARCHICAL = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.ASSET_CATEGORIES_SEARCH_HIERARCHICAL));
-	
+
+	public static final boolean ASSET_CATEGORIES_SEARCH_HIERARCHICAL = GetterUtil
+			.getBoolean(PropsUtil.get(PropsKeys.ASSET_CATEGORIES_SEARCH_HIERARCHICAL));
+
 	public AnnouncementIndexer() {
-		setDefaultSelectedFieldNames(
-			Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.CONTENT,
-			Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK, Field.GROUP_ID,
-			Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE, Field.UID);
+		setDefaultSelectedFieldNames(Field.ASSET_TAG_NAMES, Field.COMPANY_ID, Field.CONTENT, Field.ENTRY_CLASS_NAME,
+				Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID, Field.TITLE,
+				Field.UID);
 		setFilterSearch(true);
 		setPermissionAware(true);
 	}
-	
+
 	@Override
 	public String getClassName() {
 		return CLASS_NAME;
@@ -115,8 +121,10 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 		int commentsCount = 0;
 		double ratingsCount = 0;
 		if (announcement != null && !announcement.isNew()) {
-			commentsCount = MBMessageLocalServiceUtil.getDiscussionMessagesCount(Announcement.class.getName(), announcement.getAnnouncementId(), WorkflowConstants.STATUS_APPROVED);
-			RatingsStats ratingsStats = RatingsStatsLocalServiceUtil.getStats(Announcement.class.getName(), announcement.getAnnouncementId());
+			commentsCount = MBMessageLocalServiceUtil.getDiscussionMessagesCount(Announcement.class.getName(),
+					announcement.getAnnouncementId(), WorkflowConstants.STATUS_APPROVED);
+			RatingsStats ratingsStats = RatingsStatsLocalServiceUtil.getStats(Announcement.class.getName(),
+					announcement.getAnnouncementId());
 			ratingsCount = ratingsStats.getAverageScore();
 		}
 
@@ -153,20 +161,20 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 			}
 		}
 
-//		String resourcePrimKey = document.get(Field.ENTRY_CLASS_PK);
-//		portletURL.setParameter("action", "full_content");
-//		portletURL.setParameter("resourcePrimKey", resourcePrimKey);
+		// String resourcePrimKey = document.get(Field.ENTRY_CLASS_PK);
+		// portletURL.setParameter("action", "full_content");
+		// portletURL.setParameter("resourcePrimKey", resourcePrimKey);
 
 		return new Summary(title, content);
 	}
 
 	@Override
 	protected void doReindex(String className, long classPK) throws Exception {
-		
+
 		Announcement announcement = announcementLocalService.getAnnouncement(classPK);
 
 		doReindex(announcement);
-		
+
 	}
 
 	@Override
@@ -174,57 +182,47 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 		long companyId = GetterUtil.getLong(ids[0]);
 
 		reindexEntries(companyId);
-		
+
 	}
-	
+
 	protected void reindexEntries(long companyId) throws PortalException {
-		final IndexableActionableDynamicQuery indexableActionableDynamicQuery =
-				announcementLocalService.getIndexableActionableDynamicQuery();
+		final IndexableActionableDynamicQuery indexableActionableDynamicQuery = announcementLocalService
+				.getIndexableActionableDynamicQuery();
 
-		indexableActionableDynamicQuery.setAddCriteriaMethod(
-			new ActionableDynamicQuery.AddCriteriaMethod() {
+		indexableActionableDynamicQuery.setAddCriteriaMethod(new ActionableDynamicQuery.AddCriteriaMethod() {
 
-				@Override
-				public void addCriteria(DynamicQuery dynamicQuery) {
-					Property displayDateProperty = PropertyFactoryUtil.forName(
-						"displayDate");
+			@Override
+			public void addCriteria(DynamicQuery dynamicQuery) {
+				Property displayDateProperty = PropertyFactoryUtil.forName("displayDate");
 
-					dynamicQuery.add(displayDateProperty.lt(new Date()));
+				dynamicQuery.add(displayDateProperty.lt(new Date()));
 
-					Property statusProperty = PropertyFactoryUtil.forName(
-						"status");
+				Property statusProperty = PropertyFactoryUtil.forName("status");
 
-					Integer[] statuses = {
-						WorkflowConstants.STATUS_APPROVED,
-						WorkflowConstants.STATUS_IN_TRASH
-					};
+				Integer[] statuses = { WorkflowConstants.STATUS_APPROVED, WorkflowConstants.STATUS_IN_TRASH };
 
-					dynamicQuery.add(statusProperty.in(statuses));
-				}
+				dynamicQuery.add(statusProperty.in(statuses));
+			}
 
-			});
+		});
 		indexableActionableDynamicQuery.setCompanyId(companyId);
-		indexableActionableDynamicQuery.setPerformActionMethod(
-			new ActionableDynamicQuery.PerformActionMethod<Announcement>() {
+		indexableActionableDynamicQuery
+				.setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Announcement>() {
 
-				@Override
-				public void performAction(Announcement entry) {
-					try {
-						Document document = getDocument(entry);
+					@Override
+					public void performAction(Announcement entry) {
+						try {
+							Document document = getDocument(entry);
 
-						indexableActionableDynamicQuery.addDocuments(document);
-					}
-					catch (PortalException pe) {
-						if (LOGGER.isWarnEnabled()) {
-							LOGGER.warn(
-								"Unable to index announcement entry " +
-									entry.getAnnouncementId(),
-								pe);
+							indexableActionableDynamicQuery.addDocuments(document);
+						} catch (PortalException pe) {
+							if (LOGGER.isWarnEnabled()) {
+								LOGGER.warn("Unable to index announcement entry " + entry.getAnnouncementId(), pe);
+							}
 						}
 					}
-				}
 
-			});
+				});
 		indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
 
 		indexableActionableDynamicQuery.performActions();
@@ -234,15 +232,13 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 	protected void doReindex(Announcement announcement) throws Exception {
 		Document document = getDocument(announcement);
 
-		indexWriterHelper.updateDocument(
-			getSearchEngineId(), announcement.getCompanyId(), document,
-			isCommitImmediately());
-		
+		indexWriterHelper.updateDocument(getSearchEngineId(), announcement.getCompanyId(), document,
+				isCommitImmediately());
+
 	}
-	
+
 	@Override
-	public void postProcessSearchQuery(BooleanQuery searchQuery, SearchContext searchContext)
-		throws Exception {
+	public void postProcessSearchQuery(BooleanQuery searchQuery, SearchContext searchContext) throws Exception {
 
 		if (searchContext.getAttributes() == null) {
 			return;
@@ -250,14 +246,14 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 
 		addSearchLocalizedTerm(searchQuery, searchContext, Field.TITLE, true);
 	}
-	
+
 	@Override
 	protected void addSearchAssetCategoryIds(BooleanFilter booleanFilter, SearchContext searchContext)
-		throws Exception {
+			throws Exception {
 
 		addSearchAnyCategories(booleanFilter, searchContext);
 	}
-	
+
 	/**
 	 * @param contextQuery
 	 * @param searchContext
@@ -266,83 +262,73 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 	protected void addSearchAnyCategories(BooleanFilter queryBooleanFilter, SearchContext searchContext)
 			throws Exception {
 
-			PermissionChecker permissionChecker =
-				PermissionThreadLocal.getPermissionChecker();
+		PermissionChecker permissionChecker = PermissionThreadLocal.getPermissionChecker();
 
-			long[] anyCategoryIds = searchContext.getAssetCategoryIds();
+		long[] anyCategoryIds = searchContext.getAssetCategoryIds();
 
-			if (anyCategoryIds.length == 0) {
-				return;
-			}
-
-			long[] filteredAnyCategoryIds = filterCategoryIds(
-				permissionChecker, anyCategoryIds);
-
-			if (filteredAnyCategoryIds.length == 0) {
-				addImpossibleTerm(queryBooleanFilter, Field.ASSET_CATEGORY_IDS);
-
-				return;
-			}
-
-			TermsFilter categoryIdsTermsFilter = new TermsFilter(
-				Field.ASSET_CATEGORY_IDS);
-
-			for (long anyCategoryId : filteredAnyCategoryIds) {
-				AssetCategory assetCategory =
-					AssetCategoryLocalServiceUtil.fetchAssetCategory(anyCategoryId);
-
-				if (assetCategory == null) {
-					continue;
-				}
-
-				List<Long> categoryIds = new ArrayList<>();
-
-				if (ASSET_CATEGORIES_SEARCH_HIERARCHICAL) {
-					categoryIds.addAll(
-						AssetCategoryLocalServiceUtil.getSubcategoryIds(
-							anyCategoryId));
-				}
-
-				if (categoryIds.isEmpty()) {
-					categoryIds.add(anyCategoryId);
-				}
-
-				categoryIdsTermsFilter.addValues(
-					ArrayUtil.toStringArray(
-						categoryIds.toArray(new Long[categoryIds.size()])));
-			}
-
-			queryBooleanFilter.add(categoryIdsTermsFilter, BooleanClauseOccur.MUST);
+		if (anyCategoryIds.length == 0) {
+			return;
 		}
-	
-	
+
+		long[] filteredAnyCategoryIds = filterCategoryIds(permissionChecker, anyCategoryIds);
+
+		if (filteredAnyCategoryIds.length == 0) {
+			addImpossibleTerm(queryBooleanFilter, Field.ASSET_CATEGORY_IDS);
+
+			return;
+		}
+
+		TermsFilter categoryIdsTermsFilter = new TermsFilter(Field.ASSET_CATEGORY_IDS);
+
+		for (long anyCategoryId : filteredAnyCategoryIds) {
+			AssetCategory assetCategory = assetCategoryLocalService.fetchAssetCategory(anyCategoryId);
+
+			if (assetCategory == null) {
+				continue;
+			}
+
+			List<Long> categoryIds = new ArrayList<>();
+
+			if (ASSET_CATEGORIES_SEARCH_HIERARCHICAL) {
+				categoryIds.addAll(assetCategoryLocalService.getSubcategoryIds(anyCategoryId));
+			}
+
+			if (categoryIds.isEmpty()) {
+				categoryIds.add(anyCategoryId);
+			}
+
+			categoryIdsTermsFilter
+					.addValues(ArrayUtil.toStringArray(categoryIds.toArray(new Long[categoryIds.size()])));
+		}
+
+		queryBooleanFilter.add(categoryIdsTermsFilter, BooleanClauseOccur.MUST);
+	}
+
 	/**
 	 * @param permissionChecker
 	 * @param categoryIds
 	 * @return
 	 * @throws PortalException
 	 */
-	public static long[] filterCategoryIds(
-			PermissionChecker permissionChecker, long[] categoryIds)
-		throws PortalException {
+	public long[] filterCategoryIds(PermissionChecker permissionChecker, long[] categoryIds) throws PortalException {
 
-		String portalLevelClassName="com.liferay.portlet.asset.service.permission.AssetCategoryPermission";
-		String requiredPortalLevelMthodName="contains";
-		Class<?>[] parameterTypePortalArray = {PermissionChecker.class, AssetCategory.class, String.class};
-		
-				
+		String portalLevelClassName = "com.liferay.portlet.asset.service.permission.AssetCategoryPermission";
+		String requiredPortalLevelMthodName = "contains";
+		Class<?>[] parameterTypePortalArray = { PermissionChecker.class, AssetCategory.class, String.class };
+
 		List<Long> viewableCategoryIds = new ArrayList<>();
 
 		for (long categoryId : categoryIds) {
-			AssetCategory category =
-				AssetCategoryLocalServiceUtil.fetchCategory(categoryId);
-			
-			Object[] valuesPortalArray={permissionChecker, category, ActionKeys.VIEW};
-			MethodKey getSessionUsers = new MethodKey(ClassResolverUtil.resolveByPortalClassLoader(portalLevelClassName),
-					requiredPortalLevelMthodName,parameterTypePortalArray);
-			
+			AssetCategory category = assetCategoryLocalService.fetchCategory(categoryId);
+
+			Object[] valuesPortalArray = { permissionChecker, category, ActionKeys.VIEW };
+			MethodKey getSessionUsers = new MethodKey(
+					ClassResolverUtil.resolveByPortalClassLoader(portalLevelClassName), requiredPortalLevelMthodName,
+					parameterTypePortalArray);
+
 			try {
-				if ((category != null) && GetterUtil.getBoolean(PortalClassInvoker.invoke(getSessionUsers,valuesPortalArray))) {
+				if ((category != null)
+						&& GetterUtil.getBoolean(PortalClassInvoker.invoke(getSessionUsers, valuesPortalArray))) {
 					viewableCategoryIds.add(categoryId);
 				}
 			} catch (Exception e) {
@@ -350,20 +336,56 @@ public class AnnouncementIndexer extends BaseIndexer<Announcement> {
 			}
 		}
 
-		return ArrayUtil.toArray(
-			viewableCategoryIds.toArray(new Long[viewableCategoryIds.size()]));
+		return ArrayUtil.toArray(viewableCategoryIds.toArray(new Long[viewableCategoryIds.size()]));
 	}
-	
+
 	/**
 	 * @param queryBooleanFilter
 	 * @param field
 	 * @throws Exception
 	 */
-	protected void addImpossibleTerm(
-			BooleanFilter queryBooleanFilter, String field)
-		throws Exception {
+	protected void addImpossibleTerm(BooleanFilter queryBooleanFilter, String field) throws Exception {
 
 		queryBooleanFilter.addTerm(field, "-1", BooleanClauseOccur.MUST);
+	}
+	
+	@Override
+	public void postProcessSearchQuery(
+			BooleanQuery searchQuery, BooleanFilter fullQueryBooleanFilter,
+			SearchContext searchContext)
+		throws Exception {
+
+		addStatus(fullQueryBooleanFilter, searchContext);
+
+		int announcementId = GetterUtil.getInteger(searchContext.getAttribute("announcementId"));
+		long typeId = GetterUtil.getLong(searchContext.getAttribute("typeId"));
+		long regionId = GetterUtil.getLong(searchContext.getAttribute("regionId"));
+		long countryId = GetterUtil.getLong(searchContext.getAttribute("countryId"));
+		
+		boolean isUserAnnouncements = GetterUtil.getBoolean(searchContext.getAttribute("isUserAnnouncements"));;
+
+		if (announcementId != 0) {
+			searchQuery.addRequiredTerm("announcementId", announcementId);
+		}
+		if (typeId != 0) {
+			searchQuery.addRequiredTerm("typeId", typeId);
+		}
+		
+		if (regionId != 0) {
+			searchQuery.addRequiredTerm("regionId", regionId);
+		}
+		
+		if (countryId != 0) {
+			searchQuery.addRequiredTerm("countryId", countryId);
+		}
+		
+		if (isUserAnnouncements) {
+			
+			BooleanQuery userIdQuery = new BooleanQueryImpl();
+			userIdQuery.addRequiredTerm(Field.USER_ID, searchContext.getUserId());
+			searchQuery.add(userIdQuery, BooleanClauseOccur.MUST);
+		}
+		
 	}
 
 }
